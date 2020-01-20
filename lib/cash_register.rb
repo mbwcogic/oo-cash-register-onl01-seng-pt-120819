9a234eba2abc53fdd1ed3d50c9ead15d2c2027a7
Michael Waldo <mbwcogic@yahoo.com>
2020-01-20 00:55:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   end
   def add_item(title, price, quantity=1)
     self.total += (price * quantity)
-   while quanity < 0
+   while quanity > 0
     @items << title  
   end
   def apply_discount 
