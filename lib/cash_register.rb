c5a1d780e263ee868bda8e088fe7b2bb815b161c
Michael Waldo <mbwcogic@yahoo.com>
2020-01-20 00:46:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,6 +3,7 @@ class CashRegister
   def initialize(discount=0) 
     @total = 0
     @discount = discount
+    @items = []
   end
   def add_item(title, price, quantity=1)
     self.total += (price * quantity)
@@ -15,7 +16,7 @@ class CashRegister
     "There is no discount to apply."
   end
   end 
-  def item
-    @item
+  def items
+    @items
   end
 end
