291124f214e2be893932b9403610f85b05c0f062
Michael Waldo <mbwcogic@yahoo.com>
2020-01-20 01:11:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
   def items
     @items
   end
-  def void_last_transaction(price, )
+  def void_last_transaction(price, quantity)
   self.total = self.total - self.last_transaction
   end 
 end
